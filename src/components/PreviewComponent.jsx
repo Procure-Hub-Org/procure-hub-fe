193354c4461c60395e73298d1b1140d9de0b6f38
harunmioc <harun.mioc@gmail.com>
2025-04-04 01:25:54 +0200

    Added pagination and toast notification

--- src/components/PreviewComponent.jsx
+++ src/components/PreviewComponent.jsx
@@ -10,6 +10,8 @@ import CustomCheckbox from "./Input/Checkbox"; // Adjust the import path as nece
 import CustomRadioGroup from "./Input/RadioSelect"; // Adjust the import path as necessary
 import Navbar from "./Navbar/Navbar";
 import SideBar from "./SideBar/SideBar";
+import NotificationToast from "./Notifications/NotificationToast";
+import CustomPagination from "./Pagination/CustomPagination";
 
 //added customized components for preview testing
 const PreviewComponent = () => {
@@ -52,27 +54,23 @@ const PreviewComponent = () => {
     <div style={{ position: "absolute", top: "0", left: "0", width: "100vw" }}>
       <h2>Navbar</h2>
       <Navbar />
-
       <h2>SideBar button click it</h2>
       <div>
         <PrimaryButton onClick={toggleSidebar}>Toggle Sidebar</PrimaryButton>
         {/* Pass the open state and the function to close it */}
         <SideBar open={openSidebar} onClose={toggleSidebar} />
       </div>
-
       <h2>Button Previews</h2>
       <PrimaryButton>Click Me</PrimaryButton>
       <SecondaryButton>Click</SecondaryButton>
       <BasicButton>Click</BasicButton>
       <OutlinedButton>Click</OutlinedButton>
-
       {/* Custom Input Text Field */}
       <h3>Custom TextField</h3>
       <CustomTextField
         label="Enter text"
         placeholder="Type something here..."
       />
-
       {/* Custom Select Field */}
       <h3>Custom Select</h3>
       <CustomSelect
@@ -81,7 +79,6 @@ const PreviewComponent = () => {
         options={options}
         helperText="Please select an option"
       />
-
       {/* Custom Search Input */}
       <h3>Custom Search Input</h3>
       <CustomSearchInput
@@ -89,7 +86,6 @@ const PreviewComponent = () => {
         onChange={handleSearchChange}
         placeholder="Search..."
       />
-
       {/* Custom Checkbox */}
       <h3>Custom Checkbox</h3>
       <CustomCheckbox
@@ -97,7 +93,6 @@ const PreviewComponent = () => {
         checked={checkboxChecked}
         onChange={handleCheckboxChange}
       />
-
       {/* Custom Radio Group */}
       <h3>Custom Radio Group</h3>
       <CustomRadioGroup
@@ -110,6 +105,18 @@ const PreviewComponent = () => {
           { value: "option3", label: "Option 3" },
         ]}
       />
+      {/* Example of notification toast, shows up in the middle of the screen at the bottom*/}
+      <NotificationToast
+        message="Item added to cart!"
+        autoHideDuration={3000}
+      />
+      {/* onChange function can do whatever you need it to do*/}
+      <CustomPagination
+        count={10}
+        page={1}
+        onChange={(e, p) => console.log("Page:", p)}
+      />
+      ;
     </div>
   );
 };
