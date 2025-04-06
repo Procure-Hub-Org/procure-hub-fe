da8963fa0079b5550537fc36b43566f07bc67fe9
MersidPi <mpilipovic1@etf.unsa.ba>
2025-04-06 17:25:15 +0200

    Profile UI: implemented v1

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import PreviewComponent from "./components/PreviewComponent";
 import RegisterPage from "./pages/RegisterPage";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+
 function App() {
   return (
-      /* <Layout>
-        <Home />
-      </Layout>*/
-
-
-    <PreviewComponent></PreviewComponent>
-    // <RegisterPage></RegisterPage>
-    //<ComingSoon></ComingSoon>
-    
-
-
+    <>
+      {/* <Layout> */}
+      {/* <Home /> */}
+      {/* </Layout> */}
+      {/* <Login></Login> */}
+      {/* <PreviewComponent></PreviewComponent> */}
+      {/* <RegisterPage></RegisterPage> */}
+      {/* //<ComingSoon></ComingSoon> */}
+      <UserProfile></UserProfile>
+    </>
   );
 }
 
